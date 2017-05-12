ba02f1ecb9ef588ea1e234e211a9bb8c59bb5f66
USER1-PC\user <a.gjini@live.com>
2017-05-12 14:12:29 +0200

    ora 2

--- routes/rezervim.js
+++ routes/rezervim.js
@@ -172,7 +172,7 @@ exports.getOraretZene = function(req,res,next){
       console.log('Connected to postgresss! get orare');
 
       client
-        .query('SELECT id,emer,mbiemer,celular FROM clients WHERE user_id = $1;',[dataZgjdhur])
+        .query('SELECT * FROM oraret2 WHERE data = $1;',[dataZgjdhur])
          // .query('SELECT grupi,kodartikulli,kodifikimartikulli2,pershkrimartikulli FROM products2 WHERE kodartikulli = $1',[productId])
         
         .on('end', function(row) {
