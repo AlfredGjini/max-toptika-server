88a09fd9397ae1ad6bdc3de7a680479e6ac09826
USER1-PC\user <a.gjini@live.com>
2017-06-06 17:08:25 +0200

    Clinic Card 2

--- routes/clinic-card.js
+++ routes/clinic-card.js
@@ -2,28 +2,33 @@ var clinicData = [];
 var pg = require('pg');
 var connectionStr = 'postgres://dekixmpqcprkpu:MNbCC56WQ1ZaNRqX8GHmTBaUv-@ec2-23-21-55-25.compute-1.amazonaws.com:5432/d3fn4lugik4eop';
 pg.defaults.ssl = true;
-    
+
 
 exports.getClinicCard = function(req,res,next){
-    var client_id = req.body.client_id;
+  var client_id = req.body.client_id;
     var id = req.body.id;
-    // var id = req.query.id;
-    //var id =12;
-    pg.connect(connectionStr, function(err, client, done) {
+
+  pg.connect(connectionStr, function(err, client, done) {
       if (err) {
         //console.log();
         throw err;
       }
+      console.log('Connected to postgresss! get clinic card');
+      var query="SELECT * FROM clinic_card WHERE id_client='"+client_id+"' AND id='"+id+"' ORDER BY data_vizites DESC LIMIT 1";
+
       client
-        .query('SELECT * FROM clinic_card WHERE id_client=$1 AND id=$2 ORDER BY data_vizites DESC LIMIT 1',[client_id],[id])
+        .query(query)
+         // .query('SELECT grupi,kodartikulli,kodifikimartikulli2,pershkrimartikulli FROM products2 WHERE kodartikulli = $1',[productId])
+        
         .on('end', function(end) {
-          console.log(end);
-            res.send(end.rows[0]);
-            done();
-            // client.end();
-          
+          //console.log(row);
+          //console.log('Single item : ', productId);
+          res.send(end);
+          // client.end();
+          done();
         });
     });
-      
-    
+  pg.end(function(err) {
+        if (err) throw err;
+    });
 };
